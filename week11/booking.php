6fff94b0167bc94d9a4c44df3bc4c9eda481cea4
Terence Yapp <terence243051@dasein.edu.my>
2026-08-13 13:10:42 +0800

    feat: UID.php completed

--- week11/booking.php
+++ week11/booking.php
@@ -44,10 +44,10 @@ if (isset($_POST["book"])) {
         $session = mysqli_fetch_assoc($slotResult);
 
         if ($session["SessionSlots"] > 0) {
-
+2
             $insert = "INSERT INTO booking_profiles
-                       (Session_ID, Email)
-                       VALUES ('$sessionID', '$email')";
+                       (Session_ID, Email, eventname, date)
+                       VALUES ('$sessionID', '$email', '$session[SessionName]', '$session[SessionDate]')";
 
             mysqli_query($conn, $insert);
 
---
+++ week11/uid.php
@@ -0,0 +1,92 @@
+<?php
+$servername = "localhost";
+$username = "terence243051";
+$password = "wyaslwwjz030331121139YES!";
+$dbname = "terence243051";
+
+if (isset($_SESSION['email']) && isset($_SESSION['password'])) {
+}
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+date_default_timezone_set('Asia/Kuala_Lumpur');
+$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+$code = '';
+
+for ($i = 0; $i < 6; $i++) {
+    $code .= $characters[random_int(0, strlen($characters) - 1)];
+}
+
+$uniqueCode = date('YmdHis') . "_" . $code;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  $Name = $_POST["name"];
+  $Email = $_POST["Email"];
+  $Age = $_POST["Age"];
+
+    $insert = "INSERT INTO uid
+            (name, Email, Age, UID)
+            VALUES ('$Name', '$Email', '$Age', '$uniqueCode')";
+
+            mysqli_query($conn, $insert);
+
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UID</title>
+
+      <style>
+    * {
+      font-size: 20px;
+    }
+
+    body {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      flex-direction: column;
+    }
+  </style>
+
+</head>
+
+<body>
+  <form target="_self" method="POST">
+    <h2>Name:</h2>
+    <input type="text" name="name" required>
+
+    <br>
+
+    <h2>Email:</h2>
+    <input type="email" name="Email" required>
+
+    <br>
+
+    <h2>Age:</h2>
+    <input type="number" name="Age" required>
+
+    <br><br>
+
+    <input type="submit" value="Login">
+  </form>
+
+  <h2>
+    <?php echo $uniqueCode; ?>
+  </h2>
+</body>
+
+</html>
