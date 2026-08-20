5d25850a8df461556bdb63e44bf49dbd71232cba
Terence Yapp <terence243051@dasein.edu.my>
2026-08-20 21:21:58 +0800

    feat: A2 started

---
+++ DDM251_2-5_WebApplication_TerenceYappChengFei_A2/df_index.php
@@ -0,0 +1,104 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "terence243051";
+$password = "wyaslwwjz030331121139YES!";
+$dbname = "terence243051";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+if (!isset($_SESSION["email"])) { //not equal to null
+
+    header("Location:index.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week2 - Booklist</title>
+</head>
+
+<style>
+    table {
+        border-collapse: collapse;
+    }
+
+    table,
+    th,
+    td {
+        border: 1px solid black;
+    }
+</style>
+
+<body>
+    <table width="1100">
+        <tr>
+            <th>ISBN</th>
+            <th width="300">Title</th>
+            <th width="200">Author</th>
+            <th>Description</th>
+            <th>Price(RM)</th>
+        </tr>
+
+        <?php
+        $query = "SELECT * FROM booklist";
+
+        $result = mysqli_query($conn, $query);
+
+        while ($row = mysqli_fetch_assoc($result)) {
+        ?>
+
+            <tr>
+                <td><?php echo $row['ISBN']; ?></td>
+                <td><?php echo $row['title']; ?></td>
+                <td><?php echo $row['author']; ?></td>
+                <td><?php echo $row['description']; ?></td>
+                <td><?php echo $row['price']; ?></td>
+                <td>
+                    <a href="editBook.php?isbn=<?php echo $row['ISBN']; ?>">
+                        <input type='button' value='Edit'>
+                    </a>
+                </td>
+                <td>
+                    <button onclick="confirmDelete('<?php echo $row['ISBN']; ?>')">Delete
+                    </button>
+                    </a>
+                </td>
+            </tr>
+        <?php
+        }
+        mysqli_close($conn);
+        ?>
+
+        <a href="profile.php"><input type="submit" value="Profile"></a>
+        <a href="addBook.php"><input type="submit" value="AddBook"></a>
+        <a href="logout.php"><input type="submit" value="LogOut"></a>
+
+    </table>
+
+    <script>
+        function confirmDelete(ISBN) {
+            let text = "Are you sure you want to delete the book with ISBN:" + ISBN + "?";
+
+            if (confirm(text) == true) {
+
+                window.location.href = "deleteBook.php?isbn=" + ISBN;
+            }
+        }
+    </script>
+
+</body>
+
+
+</html>
